4a52543d584670d2c9d8a8c8546f92a0d53106bf
Martha Bowler <mbowler133@gmail.com>
2021-12-01 14:45:30 +0000

    Remove database

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {
-  addDbItem,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
-  deleteDbItemById,
-  addDummyNotCompleteDbItems,
-  addDummyCompleteDbItems,
-} from "./db";
-import filePath from "./filePath";
-
-// loading in some dummy items into the database
-// (comment out if desired, or change the number)
-addDummyNotCompleteDbItems(50);
-addDummyCompleteDbItems(34);
 
 const app = express();
 
@@ -30,53 +14,134 @@ dotenv.config();
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
-// API info page
-app.get("/", (req, res) => {
-  const pathToFile = filePath("../public/index.html");
-  res.sendFile(pathToFile);
-});
+const Client = require("pg").Client;
+const client = new Client({ database: "to-do-list" });
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
-  res.status(200).json(allSignatures);
-});
-
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
-});
-
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
+client.connect();
+// ROUTES
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allToDos = await client.query("SELECT * FROM todos LIMIT 100");
+    res.status(200).json({
+      status: "success",
+      data: allToDos.rows,
+    });
+  } catch (err) {
+    console.error(err);
   }
 });
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = deleteDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
+// create a todo
+app.post("/todos", async (req, res) => {
+  const { tasks, due_date } = req.body;
+  if (typeof tasks === "string" && typeof due_date === "string") {
+    const createdToDO = await client.query(
+      "INSERT INTO todos VALUES (default, $1, $2) RETURNING *",
+      [tasks, due_date]
+    );
+    res.status(201).json({
+      status: "success",
+      data: {
+        signature: createdToDO.rows, //return the relevant data (including its db-generated id)
+      },
+    });
   } else {
-    res.status(200).json(matchingSignature);
+    res.status(400).json({
+      status: "fail",
+      data: {
+        name: "A string value for tasks is required in your JSON body",
+      },
+    });
   }
 });
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
+
+// get a todo
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await client.query("SELECT * FROM todos WHERE id = $1", [id]);
+  console.log(result.rowCount);
+
+  if (result.rowCount === 1) {
+    const todo = await client.query("SELECT * FROM todos WHERE id=$1", [id]);
+    res.status(200).json({
+      status: "success",
+      data: todo.rows[0],
+    });
   } else {
-    res.status(200).json(matchingSignature);
+    res.status(404).json({
+      status: "fail",
+      data: {
+        id: "Could not find a signature with that id identifier",
+      },
+    });
+  }
+});
+
+// delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params; // params are string type
+
+  const queryResult = await client.query("DELETE FROM todos WHERE id = $1", [
+    id,
+  ]);
+  const didRemove = queryResult.rowCount === 1;
+
+  if (didRemove) {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
+    // we've gone for '200 response with JSON body' to respond to a DELETE
+    //  but 204 with no response body is another alternative:
+    //  res.status(204).send() to send with status 204 and no JSON body
+    res.status(200).json({
+      status: "success",
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      data: {
+        id: "Could not find a todo with that id identifier",
+      },
+    });
+  }
+});
+
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+  //  :id refers to a route parameter, which will be made available in req.params.id
+  const { tasks, due_date } = req.body;
+  const { id } = req.params;
+  if (typeof tasks === "string" && typeof due_date === "string") {
+    const result = await client.query("SELECT * FROM todos WHERE id = $1", [
+      id,
+    ]);
+    console.log(result.rowCount);
+
+    if (result.rowCount === 1) {
+      const updatedToDo = await client.query(
+        "UPDATE todos SET tasks = $1, due_date= $2 WHERE id= $3",
+        [tasks, due_date, id]
+      );
+      res.status(200).json({
+        status: "success",
+        data: {
+          todos: updatedToDo.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          id: "Could not find a todo with that id identifier",
+        },
+      });
+    }
+  } else {
+    res.status(404).json({
+      status: "fail",
+      data: {
+        id: "Could not find a todo with that id identifier",
+      },
+    });
   }
 });
 
